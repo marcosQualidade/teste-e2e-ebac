a3fb3ca98866a0f537543956f98b986b98a2df80
Marcos Colossi <colossimarcos34@gmail.com>
2024-05-27 02:18:13 -0300

    primeiro commit

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
 
+import { btn_comprar } from '../support/commands'
+import { msg_sucesso } from '../support/commands'
+import { link_comprar } from '../support/commands';
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -15,8 +19,62 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    
+    cy.primeiroProduto()
+ // Verifica se o link/botão de compras existe e está visivel na página
+    cy.get('body').find(link_comprar).should('exist').and('be.visible')
+ // verifica se a home de produtos foi carregada corretamente.
+    cy.get('body').find('#content').should('be.visible')
+ // verifica se o nome do produto está visivel no detalhe do produto selecionado.
+    cy.get('.product_title').should('contain', 'Abominable Hoodie')
+ // verifica se o botão para adicionar o produto ao carrinho está habilitado.
+    cy.get(btn_comprar).should('be.enabled')
+ // verifica se o produto foi adicionado com sucesso ao carrinho.
+    cy.get('.woocommerce-message').should('contain', msg_sucesso)
 
+    cy.segundoProduto()
+ // Verifica se o link/botão de compras existe e está visivel na página
+    cy.get('body').find(link_comprar).should('exist').and('be.visible')
+ // verifica se o nome do produto está visivel no detalhe do produto selecionado.
+    cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
+ // verifica se o botão para adicionar o produto ao carrinho está habilitado.
+    cy.get(btn_comprar).should('be.enabled')
+ // verifica se o produto foi adicionado com sucesso ao carrinho.
+    cy.get('.woocommerce-message').should('contain', msg_sucesso)
+
+    cy.terceiroProduto()
+ // Verifica se o link/botão de compras existe e está visivel na página
+    cy.get('body').find(link_comprar).should('exist').and('be.visible')
+ // verifica se o nome do produto está visivel no detalhe do produto selecionado.
+    cy.get('.product_title').should('contain', 'Aether Gym Pant')                             
+ // verifica se o botão para adicionar o produto ao carrinho está habilitado.
+    cy.get(btn_comprar).should('be.enabled')
+ // verifica se o produto foi adicionado com sucesso ao carrinho.
+    cy.get('.woocommerce-message').should('contain', msg_sucesso)
+
+
+    cy.quartoProduto()
+ // Verifica se o link/botão de compras existe e está visivel na página
+    cy.get('body').find(link_comprar).should('exist').and('be.visible')
+ // verifica se a home de produtos foi carregada corretamente.
+    cy.get('body').find('#content').should('be.visible')
+ // verifica se o nome do produto está visivel no detalhe do produto selecionado.
+    cy.get('.product_title').should('contain', 'Argus All-Weather Tank')                             
+ // verifica se o botão para adicionar o produto ao carrinho está habilitado.
+    cy.get(btn_comprar).should('be.enabled')
+ // verifica se o produto foi adicionado com sucesso ao carrinho.
+    cy.get('.woocommerce-message').should('contain', msg_sucesso)
+
+    cy.get('.woocommerce-message > .button').should('be.visible')
+     // valida se o botão para concluir a compra está presente.
+
+    cy.carrinho()
+ // valida se o botão para concluir a compra está presente.
+ //   cy.get('.checkout-button').should('be.visible')
+ // verifica se o endpoint do checkout está correto.  
+  //  cy.url().should('include', 'checkout')
+
+    
+  });
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,73 @@
+
+const msg_sucesso = 'foi adicionado no seu carrinho.'
+const link_comprar = '#primary-menu > .menu-item-629 > a'
+const btn_comprar = '.single_add_to_cart_button'
+
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
     cy.get('#password').type(senha, {log: false})
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('primeiroProduto', () => { 
+    let produto1 = '.post-2559 > .product-block > .caption > .meta > .infor > .name > a' 
+    
+    cy.get(link_comprar).click()
+    cy.get(produto1).should('be.visible').click()
+
+    cy.get('.button-variable-item-L').click()
+    cy.get('.button-variable-item-Red').click()
+
+    cy.get(btn_comprar).click()
+})
+
+Cypress.Commands.add('segundoProduto', () => {
+    let produto2 = '.post-3111 > .product-block > .caption > .meta > .infor > .name > a'
+
+    cy.get(link_comprar).click()
+    cy.get(produto2).should('be.visible').click()
+
+    cy.get('.button-variable-item-L').click()
+    cy.get('.button-variable-item-Black').click()
+
+    cy.get(btn_comprar).click()
+})
+
+Cypress.Commands.add('terceiroProduto', () => {
+    let produto3 = '.post-3073 > .product-block > .caption > .meta > .infor > .name > a'
+
+    cy.get(link_comprar).click()
+    cy.get(produto3).should('be.visible').click()
+
+    cy.get('.button-variable-item-36').click()
+    cy.get('.button-variable-item-Brown').click()
+
+    cy.get(btn_comprar).click()
+})
+
+Cypress.Commands.add('quartoProduto', () => {
+    let produto4 = '.post-3647 > .product-block > .caption > .meta > .infor > .name > a'
+
+    cy.get(link_comprar).click()
+    cy.get(produto4).should('be.visible').click()
+
+    cy.get('.button-variable-item-M').click()
+    cy.get('.button-variable-item-Gray').click()
+
+    cy.get(btn_comprar).click()
+})
+Cypress.Commands.add('carrinho', () => {
+    cy.get('.woocommerce-message > .button').click()
+ // valida se os produtos estão de fato adicionados no carrinho.
+    cy.fixture('produtos').then((data) => {
+       data.produtos.forEach(produto => {
+          cy.get('body').should('include.text', produto)  
+        })
+     })
+    cy.get('.checkout-button').click()
+})
+
+
+export {link_comprar} 
+export {btn_comprar}
+export {msg_sucesso}
